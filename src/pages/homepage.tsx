0309afb6f774c4c48de6dc37d96f2a72e5cd67f2
Levy <edgarlevy81@gmail.com>
2024-02-18 23:40:04 +0300

    use background image

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,10 +1,11 @@
 import MainLayout from "../layout/mainlayout"
+import HomeHero from "../components/homeHero"
 function Homepage() {
   
     return (
       <>
         <MainLayout>
-          <h1 className="text-yellow-400 ">Homepage</h1>
+          <HomeHero />
         </MainLayout>
       </>
     )
