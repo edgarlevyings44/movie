af86f1e70bb969a13ef7bbf3aa049fc29ab5b7ee
OCHIENGHerman <ochiengherman32@gmail.com>
2024-02-18 17:31:17 +0300

    add: opacity for text clarity

--- src/components/homeHero.tsx
+++ src/components/homeHero.tsx
@@ -4,6 +4,7 @@ function HomeHero() {
   return (
     <>
         <div className="hero min-h-screen" style={{backgroundImage: `url(${BackgroundImage})`}}>
+          <div className="hero-overlay bg-opacity-60"></div>
           <h1 className="text-white">Homepage</h1>
         </div>
     </>
