8ce074b01e35d2a8cee6de6ff7d46fa63b06c4ec
OCHIENGHerman <ochiengherman32@gmail.com>
2024-02-18 18:05:12 +0300

    setup:footer

--- src/layout/mainlayout.tsx
+++ src/layout/mainlayout.tsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/navbar"
+import Footer from "../components/footer"
 function MainLayout({ children }: {children: React.ReactNode}) {
     
   return (
     <>
       <Navbar>
-        {children} 
+        {children}
+        <Footer/>
       </Navbar>  
     </>
   )
