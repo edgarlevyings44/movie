3a688e92698d74ab5526ae7c75f741886dae1c2a
OCHIENGHerman <ochiengherman32@gmail.com>
2024-02-18 16:05:28 +0300

    basic drawer from navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,36 +1,39 @@
 function Navbar() {
   
 
-    return (
-      <>
-        <div className="navbar bg-lime-950">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-                </svg>
-              </div>
-              <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                  <li><a>Item 1</a></li>
-                  <li><a>Item 2</a></li>
-                </ul>
+  return (
+    <>
+      <div className="navbar bg-lime-950">
+        <div className="navbar-start">
+          <div className="drawer">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              {/* Page content here */}
+              <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
             </div>
-            <a className="btn btn-ghost text-xl">daisyUI</a>
+            <div className="drawer-side">
+              <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+              <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                {/* Sidebar content here */}
+                <li><a>Sidebar Item 1</a></li>
+                <li><a>Sidebar Item 2</a></li>
+              </ul>
+            </div>
           </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              <li><a>Item 1</a></li>
-              <li><a>Item 2</a></li>
-            </ul>
-          </div>
-          <div className="navbar-end">
-            <a className="btn">Get Started</a>
-          </div>
+          <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
-      </>
-    )
-  }
-  
-  export default Navbar
-  
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Get Started</a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
