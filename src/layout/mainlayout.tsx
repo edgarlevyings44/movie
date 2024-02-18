f3111d750c81406e4d42840259134f16c104ab9e
OCHIENGHerman <ochiengherman32@gmail.com>
2024-02-18 14:02:48 +0300

    use:background image

--- src/layout/mainlayout.tsx
+++ src/layout/mainlayout.tsx
@@ -1,10 +1,13 @@
 import Navbar from "../components/navbar"
+import BackgroundImage from "../components/backgroundImage"
 function MainLayout({ children }: {children: React.ReactNode}) {
     
   return (
     <>
-     <Navbar />
-     {children}
+      <BackgroundImage>
+        <Navbar />
+        {children}
+      </BackgroundImage>    
     </>
   )
 }
