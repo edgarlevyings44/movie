93196d883ee7bae4875bc43bc09ad27051b62de3
OCHIENGHerman <ochiengherman32@gmail.com>
2024-02-18 14:34:09 +0300

    minor: fix

--- src/components/homeHero.tsx
+++ src/components/homeHero.tsx
@@ -1,10 +1,10 @@
-import BackgroundImage from '../assets/pexels-pixabay-235985.jpg'
+import BackgroundImage from '../assets/back.jpg'
 function HomeHero() {
     
   return (
     <>
         <div className="hero min-h-screen" style={{backgroundImage: `url(${BackgroundImage})`}}>
-          <h1 className="text-yellow-400 ">Homepage</h1>
+          <h1 className="text-white">Homepage</h1>
         </div>
     </>
   )
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ function Navbar() {
 
     return (
       <>
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-lime-950">
             <a className="btn btn-ghost text-xl">daisyUI</a>
         </div>
       </>
