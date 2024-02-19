eb1c8836e13f20419a5971ba8e749cff9032e095
Levy <edgarlevy81@gmail.com>
2024-02-19 20:53:12 +0300

    footer fix

--- src/layout/mainlayout.tsx
+++ src/layout/mainlayout.tsx
@@ -1,11 +1,14 @@
 import Navbar from "../components/navbar"
+import Footer from "../components/footer"
 function MainLayout({ children }: {children: React.ReactNode}) {
     
   return (
     <>
      <Navbar>
      {children}
+     <Footer />
      </Navbar>
+
     </>
   )
 }
